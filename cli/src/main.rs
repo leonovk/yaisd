02a5c1095d42e3437bdaad8233dcb3db9ffbb947
Kirill Leonov <leonov835@yandex.ru>
2024-08-10 21:40:05 +0300

    done

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,5 +1,33 @@
+use std::env;
 use yaisd_core::process_manager;
 
+const HELP_STR: &'static str = "run without arguments will start the daemon!
+available commands:
+--help
+--version";
+
 fn main() {
-    process_manager::run();
+    let current_version = env!("CARGO_PKG_VERSION");
+    let args: Vec<String> = env::args().collect();
+    let command = parse_command(&args);
+
+    if let Some(c_str) = command {
+        if c_str == "--version" {
+            println!("{}", current_version)
+        } else if c_str == "--help" {
+            println!("{}", HELP_STR)
+        } else {
+            process_manager::run();
+        }
+    } else {
+        process_manager::run();
+    }
+}
+
+fn parse_command(args: &Vec<String>) -> Option<&String> {
+    if args.len() < 2 {
+        return None;
+    } else {
+        Some(&args[1])
+    }
 }
